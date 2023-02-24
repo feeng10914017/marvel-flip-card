0c39fec80071a7a9f44bc10212e53dedd1d8c737
Hsuan En Wu <Hsuan@shawnwu.local>
2023-02-24 11:08:26 +0800

    ✨ feat: bootstrapping a standalone application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'marvel-flip-card';
