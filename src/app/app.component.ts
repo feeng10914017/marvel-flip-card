aa8e21ab22bbede03b100e9528418d8b8711976e
Hsuan En Wu <Hsuan@shawnwu.local>
2023-02-28 03:00:56 +0800

    ✨ feat: add ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { interval, timer } from 'rxjs';
 import { FlipCardComponent } from './components/flip-card/flip-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FlipCardComponent],
+  imports: [CommonModule, NgbModule, FlipCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected readonly title = 'marvel flip card';
+
+  ngOnInit() {}
 }
